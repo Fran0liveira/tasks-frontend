15155d8e97998e419de9bc369968ee6ac1de934c
Francisco Leonardo Leite Oliveira <francisco.leonardo11@outlook.com>
2024-08-07 22:17:54 -0300

    alteracoes do properties

--- src/main/java/br/ce/wcaquino/tasksfrontend/controller/TasksController.java
+++ src/main/java/br/ce/wcaquino/tasksfrontend/controller/TasksController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
 
 import br.ce.wcaquino.tasksfrontend.model.Todo;
@@ -30,7 +31,7 @@ public class TasksController {
 		return "http://" + BACKEND_HOST + ":" + BACKEND_PORT;
 	}
 	
-	@GetMapping("")
+	@GetMapping
 	public String index(Model model) {
 		model.addAttribute("todos", getTodos());
 		if(VERSION.startsWith("build"))
